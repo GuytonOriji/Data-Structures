15674b9b141a325b6124e972a3491323283d2c2c
tgc <codingviewdmv@gmail.com>
2020-06-10 11:16:37 -0400

    upgrades

--- queue/queue.py
+++ queue/queue.py
@@ -1,8 +1,9 @@
 
 import sys
-sys.path.append("../singly_linked_list/singly_linked_list.py")
+sys.path.append("../singly_linked_list/")
+
 #need help importing other files
-# import LinkedList
+from singly_linked_list import LinkedList
 
 
 # #LinkedList 
@@ -56,8 +57,7 @@ sys.path.append("../singly_linked_list/singly_linked_list.py")
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = Stack()
-        self.remove_stack = Stack()
+        self.storage = LinkedList()
 
     def __str__(self):
         return str(self.storage.storage)
@@ -67,14 +67,11 @@ class Queue:
 
     def enqueue(self, value):
         self.size += 1
-        self.storage.push(value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         if self.size > 0:
-            self.size -= 1
-            while self.storage.size > 1:
-                self.remove_stack.push(self.storage.pop())
-            headval = self.storage.pop()
-            while self.remove_stack.size > 0:
-                self.storage.push(self.remove_stack.pop())
-            return headval
+           self.size -= 1
+           return self.storage.remove_head()
+        else:
+           return None
